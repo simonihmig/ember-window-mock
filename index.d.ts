e40d2b7c563829cbe01bb433e365937ffa5fcd2a
Christian <me@rndm.de>
2018-11-26 09:10:54 +0100

    fix(types): add missing type definitions for reset, setupWindowMock

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
 declare module 'ember-window-mock' {
   export default window;
+  export function setupWindowMock(hooks: { afterEach: (fn: () => void) => void }): void;
+  export function reset(): void;
 }
