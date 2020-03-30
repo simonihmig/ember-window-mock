0b1cc9edd7deecbeb65daec6ea83fd31d478fbf3
simonihmig <simon.ihmig@kaliber5.de>
2020-03-30 21:11:03 +0200

    Update Ember 3.17

--- config/ember-try.js
+++ config/ember-try.js
@@ -4,12 +4,21 @@ const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function() {
   return {
+    useYarn: true,
     scenarios: [
       {
         name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
             'ember-source': '~3.12.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.16',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.16.0'
           }
         }
       },
