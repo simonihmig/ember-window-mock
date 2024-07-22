edde8ff8bb4cfc966385f363586966f94ac55366
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-07-22 16:04:17 +0200

    Run addon-migrator

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'ember-window-mock' {
-  export default window;
-}
-
-declare module 'ember-window-mock/test-support' {
-  export function setupWindowMock(hooks: { afterEach: (fn: () => void) => void }): void;
-  export function reset(): void;
-}
